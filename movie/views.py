bb11604602ceb42e931015df54197040453ae572
Alejoso <atirador1@eafit.edu.co>
2025-07-31 11:50:12 -0500

    Added the name to the home page again

--- movie/views.py
+++ movie/views.py
@@ -10,7 +10,7 @@ def home(request):
         movies = Movie.objects.filter(title__icontains=searchTerm)
     else:
         movies = Movie.objects.all()
-    return render(request, 'home.html' , {'searchTerm' : searchTerm , 'movies' : movies})
+    return render(request, 'home.html' , {'searchTerm' : searchTerm , 'movies' : movies , 'name' : "Alejandro Tirado Ramírez"})
 
 def about(request):
     return render(request , 'about.html')
